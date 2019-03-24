504b21c0585de3d563fd03c2a12b3751c6428be8
Devin Crowley <devin.g.crowley@gmail.com>
2019-03-23 20:01:21 -0400

    progress on get_data()

--- datascraper/abcscrape.py
+++ datascraper/abcscrape.py
@@ -2,15 +2,16 @@
 
 # TODO : Put TODOs at the top of files in this format
 # TODO : method describing where data goes once it's got?
-# TODO : what other methods do we need in the top-level class?
+# TODO : data_dir property
 
-from abc import ABC
+from abc import ABC, abstractmethod
 import numpy as np
-
+from pathlib import Path
 
 class ABCScraper(ABC):
     def __init__(self):
-        pass
+        # Initialized on first access
+        self._data_dir = None
 
     @abstractmethod
     def get_data(self, url):
@@ -18,3 +19,18 @@ class ABCScraper(ABC):
         Return a numpy.ndarray object populated from 
         a known website pointed to by url."""
         pass
+
+    @property
+    def data_dir(self):
+        """ 
+        Top-level directory where data is stored.
+        If directory doesn't exist, create it.
+        """
+        if not self._data_dir:
+            p = Path.home() / '.data_dir'
+            if not p.is_dir():
+                p.mkdir()
+                self._data_dir = str(p)
+        return self._data_dir
+
+        
--- datascraper/kagglescrape.py
+++ datascraper/kagglescrape.py
@@ -2,17 +2,41 @@ import requests
 import numpy as np
 from abcscrape import ABCScraper
 from pathlib import Path
-
+import os
+import re
 
 class KaggleScraper(ABCScraper):
+    def __init__(self):
+        if Path(''):
+            pass  # TODO: Check if data_dir/kaggle exists. Need to make ABCSCraper.data_dir property first in `abcscrape.py`.
+
     def check_credentials():
-        if (Path.home() / ".kaggle").is_dir():
+        """
+        check if credentials exist in the home directory.
+        """
+        if (Path.home() / ".kaggle/kaggle.json").exists()
             return True
         else:
             return False
 
     def get_data(self, url):
+        
+        # Check credentials
         if check_credentials():
-            pass
+            print("Credentials check passed: ~/.kaggle/kaggle.json file exists.")
         else:
-            raise Exception("")
+            raise Exception("Credentials not set up for kaggle. For instructions see \nhttps://github.com/Kaggle/kaggle-api")
+        
+        # Get data
+        basesite = "kaggle.com/"
+        datasetUrlIndex = url.find(basesite) + len(basesite)
+        dataset = url[datasetUrlIndex:]
+
+        kaggleCommand = f"kaggle datasets download -p {where put} {dataset}"
+
+
+
+        # $ kg dataset -u <username> -p <password> -o <owner> -d <dataset>
+        # https://www.kaggle.com/mohansacharya/graduate-admissions
+        # kaggle datasets download -d mohansacharya/graduate-admissions
+        # checking kaggle cli documentation
